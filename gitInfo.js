a2d2b0b992a10eee505d76c4c47f960bff01be0d
Asel <asya8427@gmail.com>
2023-06-26 01:34:49 -0600

    Finished Module 1 Assessment

---
+++ gitInfo.js
@@ -0,0 +1,85 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+const gitDefinition = "Git is a distributed version control system that enables tracking, managing, and coordinating changes to source code and other files in collaborative software development projects."
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+const gitHubDefinition = "GitHub is a web-based platform that provides hosting for Git repositories, allowing for easier collaboration, version control, and access to various development tools and features."
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+const gitInitDefinition = "'git init' is a command used to initialize a new Git repository in a directory, creating the necessary files and structure to start tracking changes and managing versions of a project."
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+const gitCloneDefinition = "'git clone' is a command used to create a copy of a remote Git repository onto a local machine, allowing for easy access to the repository's codebase and version history."
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+const gitStatusDefinition = "'git status' is a command used to display the current state of a Git repository, showing information about modified, added, and deleted files, as well as the branch and commit status."
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+const gitAddDefinition = "'git add' is a command used to stage changes in the working directory, preparing them to be included in the next commit in the Git repository."
+const gitAddCode = 'git add .'
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+const gitCommitDefinition = "'git commit' is a command used to record the staged changes in the Git repository, creating a new commit with a unique identifier and a message describing the changes made."
+const gitCommitCode = "git commit 'initial commit'"
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+const gitPushDefinition = "'git push' is a command used to upload local commits to a remote Git repository, synchronizing the changes and making them available to others working on the same repository."
---
+++ sdlc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "The analysis of requirements in the SDLC involves gathering, documenting, and validating stakeholder needs to ensure a clear understanding and successful development of the software system."
+
+const design = "Design in the SDLC involves creating a conceptual and detailed plan that outlines the structure, components, and functionality of the software system to be developed."
+
+const implementation = "Implementation in the SDLC refers to the stage where the software system is developed according to the defined requirements and design specifications, including coding, testing, and integrating different components to create a functional product."
+
+const testingAndIntegration = "Testing and Integration in the SDLC is the phase where the developed software system is thoroughly tested to ensure its functionality, reliability, and compatibility, and different components are combined and validated to ensure proper integration and smooth operation of the system as a whole."
+
+const maintain = "Maintenance in the SDLC refers to the ongoing activities performed after the software system has been deployed, including bug fixes, enhancements, performance optimizations, and addressing user feedback, to ensure the system remains functional, secure, and aligned with changing business requirements throughout its lifecycle."
